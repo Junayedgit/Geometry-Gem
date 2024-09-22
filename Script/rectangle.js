7164caa89045774234b14f9c3d3a66868b9c7d27
Junayedgit <ahmedjonayed802@gmail.com>
2024-09-22 21:55:01 +0600

    rectangle

--- Script/rectangle.js
+++ Script/rectangle.js
@@ -3,12 +3,19 @@ function rectangleCalculator() {
     const rectangleWidthInput = document.getElementById('ractangle-width');
     const rectangleWidthText = rectangleWidthInput.value;
     const Width = parseFloat(rectangleWidthText);
-    console.log(Width);
+    // console.log(Width);
 
     // Rectangle Length
     const rectangleLengthInput = document.getElementById('rectangle-length');
     const rectangleLengthText = rectangleLengthInput.value;
     const Length = parseFloat(rectangleLengthText);
-    console.log(Length);
+    // console.log(Length);
+
+    // Calculation
+    const area = Width * Length;
+    // Display
+    const rectangleArea = document.getElementById('rectangle-area');
+    rectangleArea.innerText = area;
+    console.log(area);
 
 }
